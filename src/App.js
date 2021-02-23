bdd9e97570874f701d9b3c281cb7f0f284f1e4d4
DESKTOP-9JID4FU\O1560 <21067855@qq.com>
2021-02-23 19:35:29 +0800

    添加路由

--- src/App.js
+++ src/App.js
@@ -1,42 +1,29 @@
-import { useLayoutEffect,useRef } from 'react';
-import styles from './App.module.css';
+import React from 'react';
+import RefHook from './ref/RefHook'
+import {Link, Switch, Route, BrowserRouter as Router} from 'react-router-dom'
+
+const Home = ()=>{
+  return (
+  <div>
+    <Link to="/use-ref">1 - Learning useRef Hook</Link>
+  </div>);
+}
 
 const App = ()=> {
 
-  const inputGroupRef = useRef();
-  const inputRef = useRef();
-
-  useLayoutEffect(()=>{
-    const {current} = inputRef;
-
-    const handleFocus = ()=>{
-      return inputGroupRef.current.classList.add(styles.active);
-    };
-    const handleBlur = ()=>inputGroupRef.current.classList.remove(styles.active);
-
-    current.addEventListener('focus', handleFocus);
-    current.addEventListener('blur', handleBlur);
-
-    return ()=>{
-      current.removeEventListener('focus', handleFocus);
-      current.removeEventListener('blue', handleBlur);
-    }
-  },[])
-
   return (
-    <div className={styles.container}>
-      <div className={styles.inputGroup} ref={inputGroupRef}>
-        <label className={styles.label}>
-          Your Name:
-        </label>
-        <input
-        className={styles.input}
-        type="text"
-        ref={inputRef}
-        autoComplete="off"/>
-        <div className={styles.border}></div>
-      </div>
-    </div>
+    <Router>
+      <Switch>
+      <Route path="/use-ref"><RefHook /></Route>
+      <Route exact path="/">
+        <Home />
+      </Route>
+        
+        
+        
+      </Switch>
+    </Router>
+    
   )
 }
 
